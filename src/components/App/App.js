ab4b3d3442a694dd674c5e7f7d0570432e498383
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-11-28 13:15:04 +0100

    some animations and enter key

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,64 @@
 import React, {Component} from 'react';
 import './App.scss';
+import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
+import Flash from 'react-reveal/Flash';
+import Pulse from 'react-reveal/Pulse';
+import Jump from 'react-reveal/Jump';
 
 class WelcomeWindow extends Component {
+    keyDown = e => {
+        e.key === 'Enter' && this.props.close()
+    };
+    componentDidMount() {
+        document.addEventListener('keydown', this.keyDown)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.keyDown)
+    }
+
     render() {
         return(
             <div className='welcome'>
-                <h1>HELLO!</h1>
-                <h2>Are You ready for some math?</h2>
-                <button onClick={this.props.close}>YES!</button>
+                <Zoom top delay={0}><h1>HELLO!</h1></Zoom>
+                <Zoom delay={300} top><h2>Are You ready for some math?</h2></Zoom>
+                <Zoom delay={600} top>
+                    <button onClick={this.props.close}>YES!</button>
+                </Zoom>
             </div>
         )
     }
 }
 
 class GameOver extends Component {
+    keyDown = e => {
+        e.key === 'Enter' && this.refresh();
+    };
+    componentDidMount() {
+        document.addEventListener('keydown', this.keyDown)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.keyDown)
+    }
     refresh = () => {
         window.location.reload();
     };
     render() {
-        return <div className='game-over'><h1>GAME OVER</h1><h2>Your score: {this.props.score}</h2><button onClick={this.refresh}>AGAIN!</button></div>
+        return (
+            <div className='game-over'>
+                <Flash>
+                    <h1>GAME OVER</h1>
+                </Flash>
+                <Jump delay={1000}>
+                    <h1>Your score: {this.props.score}</h1>
+                </Jump>
+                <Pulse delay={2000} count={3}>
+                    <button onClick={this.refresh}>AGAIN!</button>
+                </Pulse>
+            </div>
+        )
     }
 }
 
@@ -38,9 +78,9 @@ class ProgressBar extends Component {
 class Operation extends Component {
     render() {
         return (
-            <div className='operation'>
-                {this.props.a} + {this.props.b}
-            </div>
+                <div className='operation'>
+                    {this.props.a} + {this.props.b}
+                </div>
         )
     }
 }
@@ -49,11 +89,23 @@ class Answer extends Component {
     handleChange = (e) => {
         this.props.answer(e.target.value);
     };
+    handleEnter = e => {
+        e.key === 'Enter' && this.props.enter();
+    };
     componentDidMount() {
         this.input.focus();
     }
     render() {
-        return <input type="number" autoComplete='off' className='answer' name='answer' value={this.props.zeroInput} onChange={this.handleChange} ref={element => this.input = element}/>
+        return <input
+            type="number"
+            autoComplete='off'
+            className='answer'
+            name='answer'
+            value={this.props.zeroInput}
+            onChange={this.handleChange}
+            ref={element => this.input = element}
+            onKeyDown={this.handleEnter}
+        />
     }
 }
 
@@ -61,13 +113,16 @@ class Info extends Component {
 
     render() {
         return (
-            <div className='score'>
-                <h1>Score: {this.props.score}</h1>
-                <h1>Level: {this.props.level}</h1>
-            </div>
+             <div className='score'>
+                 <Fade right duration={1500}>
+                 <h1>Score: {this.props.score}</h1>
+                 <h1>Level: {this.props.level}</h1>
+                 </Fade>
+             </div>
         )
     }
 }
+
 class Game extends Component {
     state = {
         score: 0,
@@ -139,7 +194,7 @@ class Game extends Component {
             <>
                 <ProgressBar width={this.state.width}/>
                 <Operation a={this.state.a} b={this.state.b}/>
-                <Answer answer={this.changeAnswer} zeroInput={this.state.answer}/>
+                <Answer answer={this.changeAnswer} zeroInput={this.state.answer} enter={this.checkAnswer}/>
                 <Info score={this.state.score} level={this.state.level}/>
             </>
         )
