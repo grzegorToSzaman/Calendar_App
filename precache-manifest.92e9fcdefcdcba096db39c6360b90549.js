9900bed002a08c98b7692120ba2802648d0e2712
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-11-29 13:55:41 +0100

    Updates

---
+++ precache-manifest.92e9fcdefcdcba096db39c6360b90549.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138882d32b6c83e14afdb45f77b80823",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "4cd2fb2fa931ccc7cae7",
+    "url": "/myapp/static/css/main.e0103802.chunk.css"
+  },
+  {
+    "revision": "75450074d9b8371febcd",
+    "url": "/myapp/static/js/2.68493ee2.chunk.js"
+  },
+  {
+    "revision": "4cd2fb2fa931ccc7cae7",
+    "url": "/myapp/static/js/main.ab4b1199.chunk.js"
+  },
+  {
+    "revision": "56936cdd511d45f9e8ba",
+    "url": "/myapp/static/js/runtime-main.5a76702e.js"
+  }
+]);
